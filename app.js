2ae65d0d173791c6f27327bf2284511cc0cf564d
Sim's Dev <simonetoa04@gmail.com>
2024-02-12 13:00:39 +0100

    Set up the connection to the DB

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-import express from 'express';
-import { router as tasks } from "./router/tasks.js";
+import express from 'express'
+import 'dotenv/config'
+import {connectDb} from './db/connect.js'
+import { router as tasks } from "./router/tasks.js"
 const app = express()
 
 //middleware
@@ -8,6 +10,13 @@ app.use(express.json())
 //routes
 app.use('/api/V1/tasks',tasks)
 
-app.listen(3000,()=>{
-    console.log('app listen on port 3000')
-})
+const start = async()=>{
+    try {
+        app.listen(3000,()=>{console.log('app is listen on port 3000...')})
+        connectDb(process.env.MONGO_URI)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
