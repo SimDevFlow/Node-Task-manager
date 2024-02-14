e009bbb90059456c5bda7033d8ff43ad44735a71
Sim's Dev <simonetoa04@gmail.com>
2024-02-14 13:35:20 +0100

    ajout de la fonction récupérer toutes les tâches

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,12 @@
 import { Task } from "../models/Task.js"
 
-const getAllTasks = (req,res)=>{
-    res.send("All tasks")
+const getAllTasks = async(req,res)=>{
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({tasks})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 const createTasks = async(req,res)=>{
     
